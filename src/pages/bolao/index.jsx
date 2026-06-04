9fe270d42c1da2e22eac7760e974be63d3bb4fe9
RizziSilva <rizzi.william@hotmail.com>
2026-06-03 21:31:44 -0300

    feat: create my groups page

--- src/pages/bolao/index.jsx
+++ src/pages/bolao/index.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { BolaoCard } from "@components";
 import "./style.scss";
 
 export function Bolao() {
+  const [code, setCode] = useState("");
+
+  function handleClick() {}
+
+  function handleChange(event) {
+    const { value } = event.target;
+
+    setCode(value);
+  }
+
   function renderBoloes() {
     return ["", ""].map(({ name, points }) => (
       <BolaoCard name="Copa do mundo" points={0} />
@@ -11,13 +22,24 @@ export function Bolao() {
   return (
     <div id="container-bolao-page">
       <div className="container-content">
-        <span className="title">Meus Boloes</span>
+        <span className="title">Meus Bolões</span>
         <div className="container-groups">{renderBoloes()}</div>
         <div className="container-join">
           <div className="container-title">
             <div className="border" />
             <span className="text">Participe de um bolão</span>
             <div className="border" />
+          </div>
+          <div className="container-content">
+            <input
+              className="input"
+              placeholder="Código do bolão"
+              onChange={handleChange}
+              value={code}
+            />
+            <button className="button" onClick={handleClick}>
+              Enviar
+            </button>
           </div>
         </div>
       </div>
