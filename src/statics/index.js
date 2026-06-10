3ed8bee3068df5d8a3fd091e926e5490af0514db
RizziSilva <rizzi.william@hotmail.com>
2026-06-09 21:57:40 -0300

    feat: add right arrow

--- src/statics/index.js
+++ src/statics/index.js
@@ -2,5 +2,6 @@ import SmallLogo from "./small-logo.png";
 import DefaultUserImage from "./default-user.jpg";
 import HomeIcon from "./home.svg?react";
 import BolaoIcon from "./bolao.svg?react";
+import RightArrow from "./right-arrow.svg?react";
 
-export { SmallLogo, DefaultUserImage, HomeIcon, BolaoIcon };
+export { SmallLogo, DefaultUserImage, HomeIcon, BolaoIcon, RightArrow };
