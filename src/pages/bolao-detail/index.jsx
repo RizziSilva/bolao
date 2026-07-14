759a0faccab61da73f60276026a594f73f6ef65a
RizziSilva <rizzi.william@hotmail.com>
2026-07-14 07:38:03 -0300

    feat: add ranking button

--- src/pages/bolao-detail/index.jsx
+++ src/pages/bolao-detail/index.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useAsyncRequest } from "@hooks";
 import { STAGES } from "@constants";
+import { RankingImage } from "@statics";
 import { poolService } from "@services";
 import { Stages, StagesSelector, UpdateButton } from "./components";
 import "./style.scss";
@@ -10,6 +11,7 @@ import "./style.scss";
 export function BolaoDetail() {
   const [pool, setPool] = useState({});
   const [selectedStage, setSelectedStage] = useState(STAGES.GROUP.id);
+  const [isShowingRanking, setIsShowingRanking] = useState(false);
   const { code } = useParams();
   const { getPoolByCode } = poolService();
   const { asyncRequest } = useAsyncRequest();
@@ -29,11 +31,42 @@ export function BolaoDetail() {
     getPoolInfo();
   }, [code]);
 
+  function handleRankingClick() {
+    setIsShowingRanking(true);
+  }
+
+  function renderRankingButton() {
+    return (
+      <button onClick={handleRankingClick} className="button ranking"></button>
+    );
+  }
+
+  function renderGuessing() {
+    return (
+      <>
+        <StagesSelector
+          selectedStage={selectedStage}
+          setSelectedStage={setSelectedStage}
+        />
+        <Stages selectedStage={selectedStage} poolId={pool.id} />
+      </>
+    );
+  }
+
+  function renderContent() {
+    if (isShowingRanking) return <div>Ranking</div>;
+
+    return renderGuessing();
+  }
+
   return (
     <div id="container-my-group-id">
       <div className="container-content">
         <div className="container-upper">
-          <span className="title">{pool.name}</span>
+          <div className="container-title">
+            <span className="title">{pool.name}</span>
+            {renderRankingButton()}
+          </div>
           <div className="container-actions">
             <UpdateButton pool={pool} />
             <div className="container-game-code">
@@ -42,11 +75,7 @@ export function BolaoDetail() {
             </div>
           </div>
         </div>
-        <StagesSelector
-          selectedStage={selectedStage}
-          setSelectedStage={setSelectedStage}
-        />
-        <Stages selectedStage={selectedStage} poolId={pool.id} />
+        {renderContent()}
       </div>
     </div>
   );
